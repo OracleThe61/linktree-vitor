6d150ad1eefc31710ce3d52a6727c9391260678b
OracleThe61 <rotiv.carlos85@gmail.com>
2025-04-02 21:15:49 -0300

    Linktree atualizado com background, link para o github e outras coisas

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import { FaGithub } from "react-icons/fa";
+
+function App() {
+
+  return (
+    <div className='container-app'>
+
+      <h1>Links do Vitor❤️</h1>
+        <a href="https://github.com/OracleThe61"className='links' target='_blank'>
+        <FaGithub />Github
+        </a>
+    </div>
+  )
+}
+
+export default App
